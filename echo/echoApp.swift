43b435b953cc9353bf5114df1fb47422445112e7
sabeel <76567283+sabeelash@users.noreply.github.com>
2026-06-26 12:06:02 +0400

    Initial Commit

---
+++ echo/echoApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoApp.swift
+//  echo
+//
+//  Created by sabeel ashraf on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct echoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ echoTests/echoTests.swift
@@ -0,0 +1,19 @@
+//
+//  echoTests.swift
+//  echoTests
+//
+//  Created by sabeel ashraf on 26/06/2026.
+//
+
+import Testing
+@testable import echo
+
+struct echoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
